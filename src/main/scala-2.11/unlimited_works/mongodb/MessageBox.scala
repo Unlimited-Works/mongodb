3a199dd1c7ba8c8d31983f3e1031664ce450ab3c
LoranceChen <wangchen@qiaobutang.com>
2016-03-13 00:02:35 +0800

    rough demo

--- src/main/scala-2.11/unlimited_works/mongodb/MessageBox.scala
+++ src/main/scala-2.11/unlimited_works/mongodb/MessageBox.scala
@@ -3,7 +3,8 @@ package unlimited_works.mongodb
 import lorance.rxscoket.session._
 import org.mongodb.scala.MongoClient
 import net.liftweb.json._
-
+import org.mongodb.scala.bson.collection.immutable.Document
+import com.mongodb.util.JSON
 //import
 /**
   * which represent a connect's message queue
@@ -11,8 +12,8 @@ import net.liftweb.json._
 object MongoDaoMain extends App{
   //mongo
   val mongoClient: MongoClient = MongoClient("mongodb://localhost:27017")
-  val mongoDB = mongoClient.getDatabase("helloworld")
-  def mongoCollection(collName: String) = mongoDB.getCollection(collName)
+//  val mongoDB = mongoClient.getDatabase("helloworld")
+//  def mongoCollection(collName: String) = mongoDB.getCollection(collName)
   //open socket
   val entrance = new ServerEntrance("127.0.0.1", 10001)
   val listen = entrance.listen
@@ -20,54 +21,92 @@ object MongoDaoMain extends App{
 
   val mongoOperationSub = reader.subscribe{ s =>
     s.map{item =>
-      val jStr = new String(item.loaded.array())
-      val json = parse(jStr)
-      val collectionName = json \ "collection"
-      val theMethod = json \ "method"//CRUD
-      val theOperators = json \ "operators"
-      val theColl = mongoCollection(collectionName.asInstanceOf[String])
+      val jsonCmd = new String(item.loaded.array())
+      execute(jsonCmd)
+    }
+  }
 
-      //do search
-      theMethod.asInstanceOf[String] match {
-        /**
-          * method: find
-          * params: {
-          *   match: {...}
-          *   project: {...}
-          * }
-          */
-        case "find" =>
-//          theColl.find(compactRender(theOperators))
+  //test execute method
+  val findTestResult = execute("""{"dataBase": "helloworld", "collection": "test", "method": "find", "params":{"match":{"name": "ugl"}}}""")
+  val docs = findTestResult.map{o => o}
+  docs.subscribe{s: Document => println(s)}
 
-        /**
-          * method: insert
-          * params: {
-          *   documents: [{...},{...}]//contains one item
-          *
-          * }
-          */
-        case "insert" =>
+  Thread.currentThread().join()
+  private def execute(load: String) = {
+    val json = parse(load)
+//    val test = (json \ "dataBase")
+    val dbName = (json \ "dataBase").values.asInstanceOf[String]
+    val collectionName = (json \ "collection").values.asInstanceOf[String]
+    val theMethod = json \ "method"//CRUD
+    val params = json \ "params"
+    val theColl = mongoClient.
+      getDatabase(dbName).
+      getCollection(collectionName)
 
-        /**
-          * method: update
-          * params: {
-          *   match: {...},
-          *   modify: {...}
-          * }
-          */
-        case "update" =>
+    theMethod.values.asInstanceOf[String] match {
+      /**
+        * method: find
+        * params: {
+        *   match: {...}
+        *   //project: {...}
+        * }
+        */
+      case "find" =>
+        val matcher = compactRender(params \ "match")
+        theColl.find(Document(matcher))
 
-        /**
-          * method: remove
-          * params: {
-          *   match: {...}
-          * }
-          */
-        case "remove" =>
+      /**
+        * method: insert
+        * params: {
+        *   documents: [{...},{...}]//contains one item
+        *   multi: "boolean"
+        * }
+        */
+      case "insert" => throw new Exception
+//        val multi = (params \ "multi").asInstanceOf[String] == "true"
+//        val documents = params \ "documents"
+//
+//        if (multi) {
+//          val js = documents.asInstanceOf[JArray]
+//          val ds = js.arr.map{ i => Document(compactRender(i))}
+//          theColl.insertMany(ds)
+//        }
+//        else {
+//          val doc = Document(compactRender(documents))
+//          theColl.insertOne(doc)
+//        }
+      /**
+        * method: update
+        * params: {
+        *   match: {...},
+        *   modify: {...}
+        * }
+        */
+      case "update" =>throw new Exception
+      /**
+        * method: remove
+        * params: {
+        *   match: {...}
+        * }
+        */
+      case "remove" =>throw new Exception
 
-        case "count" =>
-        case "aggregate" =>
-      }
+      /**
+        * method: count
+        * params: {
+        *   match: {...}
+        * }
+        */
+      case "count" =>throw new Exception
+
+      /**
+        * method:aggregate
+        * params: [
+        *   {...},{...}
+        *   ...
+        * ]
+        */
+      case "aggregate" =>throw new Exception
     }
   }
 }
